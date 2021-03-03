80293c3edc72369f66287f6991d8ece84993e52a
Janelle McGill <you@example.com>
2021-03-03 18:29:57 -0500

    weds update

--- js/script.js
+++ js/script.js
@@ -27,13 +27,18 @@ function handleGetData(event) {
 }
 
 function render() {
-    $author.text(bookData.volumeInfo.authors);
-    $category.text(bookData.volumeInfo.categories);
-    $description.text(bookData.volumeInfo.description);
-    $length.text(bookData.volumeInfo.pageCount);
-    $rating.text(bookData.volumeInfo.averageRating);
-    $published.text(bookData.volumeInfo.publishedDate);
+    console.log(bookData);  
+    $author.text(bookData.volumeInfo && bookData.volumeInfo.authors ? bookData.volumeInfo.authors[0] : "No author available");
+    $category.text(bookData.volumeInfo && bookData.volumeInfo.categories ? bookData.volumeInfo.categories : "No category available");
+    $description.text(bookData.volumeInfo && bookData.volumeInfo.description ? bookData.volumeInfo.description : "No description available");
+    $length.text(bookData.volumeInfo && bookData.volumeInfo.pageCount ? bookData.volumeInfo.pageCount : "No page count available");
+    $rating.text(bookData.volumeInfo && bookData.volumeInfo.averageRating ? bookData.volumeInfo.averageRating : "No rating available");
+    $published.text(bookData.volumeInfo && bookData.volumeInfo.publishedDate ? bookData.volumeInfo.publishedDate : "No published date available");
  }
+
+
+
+ // optional chaining and nullishh collition. basically what you have to do is create a function for each result. so if there's no author. you have to create a function that's like if this parameter doesn't exist, still run this code. 
 
 
 
